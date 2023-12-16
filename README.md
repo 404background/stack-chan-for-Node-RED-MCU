3ebdfa7d0073625aa69a0bdadbec5213bfbb0941
404background <404background@gmail.com>
2023-12-16 11:19:02 +0900

    ノード作成

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # stack-chan-for-Node-RED-MCU
+## はじめに
+ｽﾀｯｸﾁｬﾝはししかわさんが開発、公開している、 手乗りサイズのｽｰﾊﾟｰｶﾜｲｲコミュニケーションロボットです。  
+作品ページ：https://github.com/meganetaaan/stack-chan  
+
---
+++ stack-chan.js
@@ -0,0 +1,10 @@
+module.exports = function(RED) {
+    function StackChan(config) {
+        var node = this;
+        RED.nodes.createNode(this, config);
+        node.on('input', function(msg) {
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("stack_chan", StackChan);
+}
